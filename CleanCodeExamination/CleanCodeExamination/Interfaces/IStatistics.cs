089555bd531ab3527c8edc8fa47eadff55f46ea2
Tonny <tonny035@hotmail.com>
2021-11-27 17:49:18 +0100

    Implement repository

--- CleanCodeExamination/CleanCodeExamination/Interfaces/IStatistics.cs
+++ CleanCodeExamination/CleanCodeExamination/Interfaces/IStatistics.cs
@@ -1,6 +1,11 @@
-﻿namespace CleanCodeExamination.Interfaces
+﻿using CleanCodeExamination.Models;
+using System.Linq;
+
+namespace CleanCodeExamination.Interfaces
 {
     public interface IStatistics
     {
+        IOrderedEnumerable<PlayerData> GetSortedTopList();
+        void SaveGame(string name, int numGuesses);
     }
 }
--- CleanCodeExamination/CleanCodeExamination/Repositories/MooGameRepository.cs
+++ CleanCodeExamination/CleanCodeExamination/Repositories/MooGameRepository.cs
@@ -1,8 +1,44 @@
 ﻿using CleanCodeExamination.Interfaces;
+using CleanCodeExamination.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
 
 namespace CleanCodeExamination.Repositories
 {
     public class MooGameRepository : IStatistics
     {
+        public IOrderedEnumerable<PlayerData> GetSortedTopList()
+        {
+            StreamReader input = new("result.txt");
+            List<PlayerData> results = new();
+            string line;
+            while ((line = input.ReadLine()) != null)
+            {
+                string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
+                string name = nameAndScore[0];
+                int guesses = Convert.ToInt32(nameAndScore[1]);
+                PlayerData data = new PlayerData(name, guesses);
+                int pos = results.IndexOf(data);
+                if (pos < 0)
+                {
+                    results.Add(data);
+                }
+                else
+                {
+                    results[pos].Update(guesses);
+                }
+            }
+            input.Close();
+            return results.OrderBy(p => p.Average());
+        }
+
+        public void SaveGame(string name, int numGuesses)
+        {
+            StreamWriter output = new("result.txt", append: true);
+            output.WriteLine(name + "#&#" + numGuesses);
+            output.Close();
+        }
     }
 }
