7d964f64fcfdd4e9aebbce8318813b1e0c242761
Tonny <tonny035@hotmail.com>
2021-12-07 10:20:13 +0100

    mooGame Init tests

---
+++ CleanCodeExamination/CleanCodeExaminationTests/Games/MooGameTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CleanCodeExamination.Games
+{
+    [TestClass()]
+    public class MooGameTests
+    {
+        [TestMethod()]
+        public void CreateGoalTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckGuessTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
